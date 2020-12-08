d434e4bb5ad12187fe1a80fdcfc0169a52bc2b11
Arneelus Lewis <arneelus777@gmail.com>
2020-12-08 14:53:37 -0500

    added gulp files to test..

---
+++ src/components/bar.js
@@ -0,0 +1,9 @@
+class Bar {
+  print(msg) {
+    document.write(msg);
+  }
+}
+
+const print02 = new Bar();
+
+// print02.print("Class Bar");
---
+++ src/components/foo.js
@@ -0,0 +1,10 @@
+class Foo {
+  log(msg) {
+    console.log(msg);
+  }
+}
+// const water = "water";
+
+// let fruits = ["apple", "banana", "grapes"];
+
+// console.log(...fruits);
